7efa4e3e181bca8585d3f2be4f26443100ccc67c
alejandro <alejimenezgitgithub@gmail.com>
2020-05-25 23:05:14 +0200

    OneUser and updates in user cruds

--- models/User.js
+++ models/User.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name:       {type: String, required: true},
-    surnames:   {type: String, required: true},
-    email:      {type: String, required: true, unique: true},
-    password:   {type: String, required: true},
+    name:               {type: String, required: true},
+    surnames:           {type: String, required: true},
+    email:              {type: String, required: true, unique: true},
+    password:           {type: String, required: true},
+    nativeLanguages:    {type: Array},
+    comunications:      {type: Array}
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/user.js
+++ routes/user.js
@@ -23,8 +23,9 @@ router.get('/logout', (req, res, next) => {
 	dscrip:  if user is connected
 */
 router.get('/whouseris', (req, res, next) => {
+	const { _id } = req.session.currentUser;
 	if (req.session.currentUser) {
-		res.status(200).json(req.session.currentUser);
+		res.status(200).json({ _id });
 	} else {
 		res.status(401).json({ code: 'unauthorized' });
 	}
@@ -71,8 +72,8 @@ router.get('/all', checkIfLoggedIn,  async (req, res, next) => {
 	body:    all params (body)
 */
 router.post('/add', async (req, res, next) => {
-	const { name, surnames, email, password } = req.body;
-	console.log({ name, surnames, email, password });
+	const { name, surnames, email, password, nativeLanguages, comunications } = req.body;
+	console.log({ name, surnames, email, password, nativeLanguages, comunications })
 	const newUser = new Users(req.body);
 	try{
 		const user = await Users.findOne({ email });
@@ -136,12 +137,31 @@ router.delete('/delete/:id', async (req, res, next) => {
 	dscrip:  get an user
 	body:    email, password
 */
+router.get('/:id', async (req, res, next) => {
+	const { id } = req.params;
+	try{
+		const user = await Users.findById(id);
+		if (user) {
+			const { _id, name, surnames, email, nativeLanguages, comunications } = user
+			return res.json( {_id, name, surnames, email, nativeLanguages, comunications } );
+		}
+		return res.status(404).json({ code: 'not-found' });
+	} catch(error) {
+		next(error);
+	}
+});
+
+/*
+	path:    /user/
+	dscrip:  get an user
+	body:    email, password
+*/
 router.post('/', checkUserEmpty, async (req, res, next) => {
 	const { email,password } = res.locals.auth;
 	try{
 		const user = await Users.findOne({ email });
 		if (user && bcrypt.compareSync(password, user.password)) {
-				req.session.currentUser = user;
+				req.session.currentUser = user._id;
 				return res.json(user);
 		}
 		return res.status(404).json({ code: 'not-found' });
@@ -149,5 +169,7 @@ router.post('/', checkUserEmpty, async (req, res, next) => {
 		next(error);
 	}
 });
+
+
 
 module.exports = router;
